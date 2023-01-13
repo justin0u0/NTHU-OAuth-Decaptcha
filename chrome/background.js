1f88f9e38030dc8b44866cc58ef985721446b483
justin0u0 <mail@justin0u0.com>
2023-01-14 03:17:07 +0800

    refactor: cleanup code

--- chrome/background.js
+++ chrome/background.js
@@ -1,46 +1,42 @@
+const decaptcha = async(maxRetries = 10) => {
+	for (let i = 0; i < maxRetries; ++i) {
+		if (typeof tf === 'undefined') {
+			await new Promise(resolve => setTimeout(resolve, 100));
+		}
+	}
 
-const decaptcha = async() => {
 	const model = await tf.loadLayersModel('https://raw.githubusercontent.com/justin0u0/NTHU-OAuth-Decaptcha/master/cnn-decaptcha/model/tensorflowjs/model.json');
 
 	const imageElement = document.getElementById('captcha_image');
 
-	let captchaCode = '';
-
-	try {
-		const imageTensor = tf.browser.fromPixels(imageElement);
-		const imageArray = imageTensor.arraySync();
-		for (const row of imageArray) {
-			for (const pixel of row) {
-				pixel[0] /= 255.0;
-				pixel[1] /= 255.0;
-				pixel[2] /= 255.0;
-			}
+	const imageTensor = tf.browser.fromPixels(imageElement);
+	const imageArray = imageTensor.arraySync();
+	for (const row of imageArray) {
+		for (const pixel of row) {
+			pixel[0] /= 255.0;
+			pixel[1] /= 255.0;
+			pixel[2] /= 255.0;
 		}
-
-		const expandedImageTensor = tf.expandDims(tf.tensor(imageArray), 0);
-
-		const tensors = model.predict(expandedImageTensor);
-		for (const tensor of tensors) {
-			const [predictions] = tensor.arraySync();
-			if (predictions.length !== 10) {
-				return;
-			}
-
-			let maxIndex = 0;
-			let maxValue = 0;
-
-			for (let i = 0; i < 10; ++i) {
-				if (predictions[i] > maxValue) {
-					maxIndex = i;
-					maxValue = predictions[i];
-				}
-			}
-
-			captchaCode += `${maxIndex}`;
-		}
-	} catch (err) {
-		console.log('err', err);
 	}
+
+	const expandedImageTensor = tf.expandDims(tf.tensor(imageArray), 0);
+
+	const tensors = model.predict(expandedImageTensor);
+
+	const captchaCode = tensors.map(tensor => {
+		const [predictions] = tensor.arraySync();
+		let maxIndex = 0;
+		let maxValue = 0;
+
+		for (let i = 0; i < 10; ++i) {
+			if (predictions[i] > maxValue) {
+				maxIndex = i;
+				maxValue = predictions[i];
+			}
+		}
+
+		return `${maxIndex}`;
+	}).join('');
 
 	document.getElementById('captcha_code').value = captchaCode;
 };
@@ -52,33 +48,14 @@ const requestFilter = {
 };
 
 // on every captcha request send, insert the captcha code automatically
-// https://developer.chrome.com/docs/extensions/reference/webRequest/#event-onBeforeSendHeaders
-chrome.webRequest.onBeforeSendHeaders.addListener(async(details) => {
-	await chrome.scripting.executeScript({
-		target: {
-			tabId: details.tabId
-		},
-		files: ['./js/tfjs-core.min.js', './js/tfjs-backend-webgl.min.js', './js/tfjs-layers.min.js'],
-	});
-
+// https://developer.chrome.com/docs/extensions/reference/webRequest/#event-onCompleted
+chrome.webRequest.onCompleted.addListener(async(details) => {
 	const injection = {
 		func: decaptcha,
-		args: [],
 		target: {
 			tabId: details.tabId
 		}
 	};
 
 	await chrome.scripting.executeScript(injection);
-}, requestFilter, ['requestHeaders', 'extraHeaders']);
-
-const urlFilter = {
-	urls: [
-		{ urlPrefix: 'https://oauth.ccxp.nthu.edu.tw/v1.1/captchaimg.php' }
-	]
-};
-
-chrome.webNavigation.onBeforeNavigate.addListener((details) => {
-	console.log('webNavigation.onBeforeNavigate', details);
-	// workaround to activate service worker
-}, urlFilter);
+}, requestFilter);
